eb8ca874c731c1600ee516fa11116e3ca2403f5e
svvolynkin@gmail.com <sergey.volynkin@flatstack.com>
2018-07-25 08:53:57 +0300

    checkpoint

--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
+
+import seedVessels from './mockData/seedVessels'
 
 Meteor.startup(() => {
-  // code to run on server at startup
-});
+  const Vessels = new Mongo.Collection('vessels')
+  seedVessels.forEach(vessel => Vessels.insert(vessel))
+
+  // TODO: Continue here
+  console.log(Vessels.find().fetch()[0])
+})
