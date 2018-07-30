39b5521b8252320e1169b6c9ec7cc6f2ee63d96c
Sergey.Volynkin <SVVolynkin@gmail.com>
2018-07-30 10:57:02 +0300

    refactored aprs api

--- imports/api/aprs.js
+++ imports/api/aprs.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { Meteor } from 'meteor/meteor'
+import { Vessels } from './vessels'
+import { APRS_FI_API_KEY } from '../../Keys'
 
 const aprs = axios.create({
   baseURL: 'https://api.aprs.fi/api/get',
@@ -6,4 +9,30 @@ const aprs = axios.create({
   params: {} // Add params later in the config
 })
 
-export default aprs
+Meteor.methods({
+  async getVesselInfo (vesselId) {
+    const mmsi = Vessels.findOne({_id: vesselId}, {fields: {MMSI: 1}}).MMSI
+
+    const response = await aprs.get('', {
+      params: {
+        name: mmsi, // use 275304000 to debug
+        what: 'loc',
+        apikey: APRS_FI_API_KEY,
+        format: 'json'
+      }
+    })
+
+    // TODO: some checks
+    const result = {
+      vessel: {
+        title: response.data.entries[0].name
+      },
+      coordinates: {
+        lat: Number(response.data.entries[0].lat),
+        lng: Number(response.data.entries[0].lng)
+      }
+    }
+
+    return result
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,38 +1,10 @@
 import { Meteor } from 'meteor/meteor'
-import aprs from '../imports/api/aprs'
 import seedVessels from './mockData/seedVessels'
 import {Vessels} from '../imports/api/vessels'
-import {APRS_FI_API_KEY} from '../Keys'
+
+import '../imports/api/aprs'
 
 Meteor.startup(() => {
   // initialize collection with fake data
   seedVessels.forEach(vessel => Vessels.insert(vessel))
-})
-
-Meteor.methods({
-  async getVesselInfo (vesselId) {
-    const mmsi = Vessels.findOne({_id: vesselId}, {fields: {MMSI: 1}}).MMSI
-
-    const response = await aprs.get('', {
-      params: {
-        name: mmsi, // use 275304000 to debug
-        what: 'loc',
-        apikey: APRS_FI_API_KEY,
-        format: 'json'
-      }
-    })
-
-    // TODO: some checks
-    const result = {
-      vessel: {
-        title: response.data.entries[0].name
-      },
-      coordinates: {
-        lat: Number(response.data.entries[0].lat),
-        lng: Number(response.data.entries[0].lng)
-      }
-    }
-
-    return result
-  }
 })
